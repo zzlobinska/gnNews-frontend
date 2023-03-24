fdf5ba2a220009506ca2a054501a3332d3c5fcb3
Zuza <z.j.zlobinska@gmail.com>
2023-03-24 17:11:20 +0100

    go back from error page

--- src/components/layout/Button/index.tsx
+++ src/components/layout/Button/index.tsx
@@ -5,11 +5,14 @@ import style from './Button.module.scss';
 type ButtonPropsType = {
   title: string;
   className?: string;
+  onClick?: () => void;
 };
 
-const Button = ({ title, className }: ButtonPropsType) => {
+const Button = ({ title, className, onClick }: ButtonPropsType) => {
   return (
-    <button className={classNames(style.button, className)}>{title}</button>
+    <button onClick={onClick} className={classNames(style.button, className)}>
+      {title}
+    </button>
   );
 };
 
--- src/features/ErrorPage/index.tsx
+++ src/features/ErrorPage/index.tsx
@@ -1,7 +1,17 @@
-import style from './ErrorPage.module.scss'
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from 'src/components';
+
+import style from './ErrorPage.module.scss';
 
 const ErrorPage = () => {
- return  <p className={style.errorText}>404</p>
-}
+  const navigate = useNavigate();
+  return (
+    <div className={style.container}>
+      <p className={style.errorText}>404</p>
+      <Button onClick={() => navigate(-1)} title='Powrót' />
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
