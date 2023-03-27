e0171bc56acbee90443710d04814eb4a7816c7f3
Zuza <z.j.zlobinska@gmail.com>
2023-03-27 09:51:30 +0200

    error page for no articles found

--- src/features/ArticlesList/index.tsx
+++ src/features/ArticlesList/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import classNames from 'classnames';
 
 import { ArticlesApi } from 'src/api';
@@ -12,7 +12,6 @@ import ArticleTile from './components/ArticleTileItem';
 import { setArticlesCount } from './slice';
 
 import style from './ArticlesList.module.scss';
-
 
 const ArticlesList = () => {
   const [articles, setArticles] = useState<ArticleType[]>([]);
@@ -41,13 +40,19 @@ const ArticlesList = () => {
 
   return (
     <div className={classNames(style.content, { [style.tiles]: !showAsList })}>
-     
-      {articles.map((article) =>
-        showAsList ? (
-          <ArticleListItem key={article.url} article={article} />
-        ) : (
-          <ArticleTile key={article.url} article={article} />
+      {articles.length > 0 ? (
+        articles.map((article) =>
+          showAsList ? (
+            <ArticleListItem key={article.url} article={article} />
+          ) : (
+            <ArticleTile key={article.url} article={article} />
+          )
         )
+      ) : (
+        <div className={style.noArticles}>
+          <p>Brak artykułów.</p>
+          <Link className={style.back} to='/'>Strona główna</Link>
+        </div>
       )}
     </div>
   );
